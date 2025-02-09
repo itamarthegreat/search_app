02f2ee3856e2651ab6fc99502bb4921499d0ce44
itamarthegreat <84128744+itamarthegreat@users.noreply.github.com>
2025-02-09 17:11:48 +0200

    Update msalConfig.ts

--- lib/msalConfig.ts
+++ lib/msalConfig.ts
@@ -2,8 +2,8 @@ import { Configuration } from '@azure/msal-browser';
 
 export const msalConfig: Configuration = {
   auth: {
-    clientId: process.env.NEXT_PUBLIC_AZURE_CLIENT_ID!,
-    authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_AZURE_TENANT_ID}`,
-    redirectUri: process.env.NEXT_PUBLIC_AZURE_REDIRECT_URI,
+    clientId:"d3ab175c-5410-409d-a1ef-e12259b63a82"!,
+    authority: `https://login.microsoftonline.com/${74e5a016-cd97-49e0-94c9-d46227757360}`,
+    redirectUri: "https://search-app-oh2u.vercel.app/",
   },
 };

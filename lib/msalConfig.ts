cf81f846e661fd199d35f6c34596460b3618dc3d
itamarthegreat <84128744+itamarthegreat@users.noreply.github.com>
2025-02-09 17:13:13 +0200

    Update msalConfig.ts

--- lib/msalConfig.ts
+++ lib/msalConfig.ts
@@ -3,7 +3,7 @@ import { Configuration } from '@azure/msal-browser';
 export const msalConfig: Configuration = {
   auth: {
     clientId:"d3ab175c-5410-409d-a1ef-e12259b63a82"!,
-    authority: `https://login.microsoftonline.com/${74e5a016-cd97-49e0-94c9-d46227757360}`,
+    authority: `https://login.microsoftonline.com/74e5a016-cd97-49e0-94c9-d46227757360`,
     redirectUri: "https://search-app-oh2u.vercel.app/",
   },
 };

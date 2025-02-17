3b1f92b36d5e1a27e7a060c307c80fb6cf3cd848
itamarthegreat <84128744+itamarthegreat@users.noreply.github.com>
2025-02-17 13:09:48 +0000

    Update @azure/msal-browser to resolve dependency conflict

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PublicClientApplication } from "@azure/msal-browser";
+
+const msalConfig = {
+    auth: {
+        clientId: "161af68c-18c5-4c56-b4bd-52cba27b77b7",
+        authority: "https://login.microsoftonline.com/74e5a016-cd97-49e0-94c9-d46227757360",
+        redirectUri: "https://search-app-oh2u.vercel.app/",
+    },
+    cache: {
+        cacheLocation: "localStorage",
+        storeAuthStateInCookie: true,
+    }
+};
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+export default msalInstance;
